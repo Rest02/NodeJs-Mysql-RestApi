6822aa00cd94288b23da55696141c0a7efc0fa1e
Rest <code.rest.dev@gmail.com>
2024-03-08 19:32:44 -0300

    0.0.2 Endpoints

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.listen(3000)
-console.log("Servidor corriendo en el puerto 3000")
+app.get("/employees", (req, res) => {
+  res.send("Obteniendo empleados");
+});
+
+app.post("/employees", (req, res) => {
+  res.send("Creando empleados");
+});
+
+app.put("/employees", (req, res) => {
+  res.send("Actualizando empleados");
+});
+
+app.delete("/employees", (req, res) => {
+  res.send("Eliminando empleados");
+});
+
+app.listen(3000);
+console.log("Servidor corriendo en el puerto 3000");
