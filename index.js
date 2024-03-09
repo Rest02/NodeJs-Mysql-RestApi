61d0bca9596651a64b69df5db427a9effd270ba4
Rest <code.rest.dev@gmail.com>
2024-03-09 20:57:41 -0300

    0.0.3 Conexion a base de datos

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from "express";
+import {pool} from './db.js'
 
 const app = express();
+
+app.get("/ping" , async (req, res) => {
+  const [result] = await pool.query("SELECT 1+1 AS RESULT")
+  res.send(result[0])
+})
 
 app.get("/employees", (req, res) => {
   res.send("Obteniendo empleados");
