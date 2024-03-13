6b5e17a7c86b8ae8dbe6918a8e25a42437ba64ff
Rest <code.rest.dev@gmail.com>
2024-03-12 23:00:52 -0300

    0.0.6 POST employee - Insertar datos

--- src/controllers/employees.controllers.js
+++ src/controllers/employees.controllers.js
@@ -1,6 +1,17 @@
+import { request } from "express";
+import {pool} from '../db.js'
+
 export const getEmployees = (req, res) => res.send("Obteniendo empleados");
 
-export const createEmployee = (req, res) => res.send("Creando empleados");
+export const createEmployee = async (req, res) => {
+    const {name , salary} = req.body
+    const [rows] = await pool.query("INSERT INTO employee(name , salary) VALUES(?,?)", [name , salary])
+    res.send({
+        id: rows.insertId,
+        name, 
+        salary
+    })
+};
 
 export const updateEmployee = (req, res) => res.send("Actualizando empleados");
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import express from "express";
+import express, { json } from "express";
 import employeesRoutes from "./routes/employees.routes.js";
 import indexRoutes from "./routes/index.routes.js";
 
 const app = express();
+
+app.use(express.json())
 
 app.use(indexRoutes);
 app.use(employeesRoutes);
