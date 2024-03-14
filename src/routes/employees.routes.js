186f58a0004389079b45718fd0a5538ef6f4a804
Rest <code.rest.dev@gmail.com>
2024-03-13 22:46:51 -0300

    0.0.8 Get employee by ID - Buscar empleado por id

--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -4,11 +4,14 @@ import {
   createEmployee,
   updateEmployee,
   deleteEmployee,
+  getEmployee,
 } from "../controllers/employees.controllers.js";
 
 const router = Router();
 
 router.get("/employees", getEmployees);
+
+router.get("/employees/:id", getEmployee);
 
 router.post("/employees", createEmployee);
 
