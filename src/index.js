7972c4147258d88264dd7c5a9271688f675523a1
Rest <code.rest.dev@gmail.com>
2024-03-16 22:30:47 -0300

    0.1.4 Variables de entorno

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
-import express, { json } from "express";
-import employeesRoutes from "./routes/employees.routes.js";
-import indexRoutes from "./routes/index.routes.js";
+import app from './app.js'
+import {PORT} from './config.js'
 
-const app = express();
-
-app.use(express.json())
-
-app.use(indexRoutes);
-app.use("/api", employeesRoutes);
-
-app.use((req, res, next)=>{
-    res.status(404).json({
-        message: "endpoint not found"
-    })
-})
-
-app.listen(3000);
-console.log("Servidor corriendo en el puerto 3000");
+app.listen(PORT);
+console.log("Servidor corriendo en el puerto", PORT);
